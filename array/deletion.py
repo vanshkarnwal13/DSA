fc9d925d31f8cbcb7607f9c5eebcb8f450cf8739
vansh Karnwal <karnwalvanshu@gmail.com>
2022-07-27 01:02:40 +0530

    update

--- array/deletion.py
+++ array/deletion.py
@@ -1,13 +1,13 @@
 a=[]
 n=int(input("Number of elements in array:"))
 for i in range(0,n):
-   l=int(input())
+   l=int(input(""))
    a.append(l)
 print(a)
 size=len(a)
 #deletion
 pos=int(input("enter position at which u want to delete"))
-for i in range(size-1,pos-1,-1):
-    a[i+1]=a[i]
-a[pos-1]=ele
+for i in range(pos-1,size-1,1):
+    a[i]=a[i+1]
+a.pop(size-1)
 print(a)
